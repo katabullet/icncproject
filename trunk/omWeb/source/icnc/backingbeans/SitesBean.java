b20a0fd11c10a6328bba36aa691b93e3e9a20e1d
lil.priincess@hotmail.com <lil.priincess@hotmail.com@695f14ab-26d0-4c50-80ea-dc42bec9b9ff>
2011-07-28 07:11:33 +0000

    OBERFLäCHE!!!

---
+++ trunk/omWeb/source/icnc/backingbeans/SitesBean.java
@@ -0,0 +1,53 @@
+package icnc.backingbeans;
+
+
+public class SitesBean{
+	
+	private int role = 4;
+	private  Integer width;
+	Boolean render = false;
+	
+		public Integer getTabWidth(){
+			switch (role) {
+				case 1:
+				{
+					width = 120;
+				}
+					break;
+				case 2:
+				{
+					width = 148;
+				}				
+					break;
+				case 3:
+				{
+					width = 188;
+				}				
+					break;
+				case 4:
+				{
+					width = 248;
+				}				
+					break;
+			}
+			return  width;
+		}
+		
+		public Boolean getRendered5(){
+			render = role>3 ? false:true;
+			
+			return render;
+		}
+		public Boolean getRendered6(){
+			render = role>2 ? false:true;
+			
+			return render;
+		}
+		public Boolean getRendered7(){
+			render = role>1 ? false:true;
+			
+			return render;
+		}
+		
+		
+}
